ccde4f5560b9ad0c85948af0c236245be242b463
Roman Klymus <klimusr@gmail.com>
2022-02-09 11:44:44 +0200

    init commit

---
+++ src/main/java/com/rklymus/diplomaconcertservice/DiplomaConcertServiceApplication.java
@@ -0,0 +1,11 @@
+package com.rklymus.diplomaconcertservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaConcertServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaConcertServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rklymus/diplomaconcertservice/DiplomaConcertServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rklymus.diplomaconcertservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaConcertServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
